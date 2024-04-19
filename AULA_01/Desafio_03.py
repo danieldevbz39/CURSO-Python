fceb05470824874260b4b8b6b8eedde0324bcf78
Daniel Siqueira <danielbzdigital@outlook.com>
2024-04-19 17:16:24 -0300

    desafios 01 a 06

--- AULA_01/Desafio_03.py
+++
@@ -1,7 +0,0 @@
-x = int(input (' Digite o primeiro numero '))
-
-
-y = int(input (' Digite o segundo numero '))
-
-
-print ('O resultado é ' , x+y)
---
+++ Desafio_03.py
@@ -0,0 +1,7 @@
+x = int(input (' Digite o primeiro numero: '))
+
+
+y = int(input (' Digite o segundo numero: '))
+
+s = x+y
+print ('O resultado é ' , s)
