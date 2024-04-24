b99051d568b6273f90173350e8c680bce91dc7a8
Daniel Siqueira <danielbzdigital@outlook.com>
2024-04-23 22:42:42 -0300

    aula 08 e exercicios p1

---
+++ aula 08/a1.py
@@ -0,0 +1,12 @@
+#from math import sqrt
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+
+#import random
+#n = random.randint(1, 12)
+#print(n)
+
+import emoji
+
+print(emoji.emojize("Olá, Mundo :🫣😝"))
