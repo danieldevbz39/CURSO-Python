c0384cf8e1be6c9a6b93abd8eddb94c693df2fd4
Daniel Siqueira <danielbzdigital@outlook.com>
2024-04-24 20:41:36 -0300

    tratamento de str

---
+++ aula 09/a1.py
@@ -0,0 +1,6 @@
+frase = 'Curso em video Python'
+print (frase.count("Curso"))
+print(frase.upper().count('Curso'))
+print(len(frase))
+print(frase.replace('Python','Android'))
+print(frase.split())
---
+++ aula 09/ex23_Separando_digitos_de_um_numero.py
@@ -0,0 +1,6 @@
+print('Digite um número: ')
+
+print('Unidade: {}'.format())
+print('Dezena: {}'.format())
+print('Centena {}'.format())
+print('Milhar: {}'.format())
