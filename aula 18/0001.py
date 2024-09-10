ddcddbea81650ab613a2039204945fa953d03bf2
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2024-09-10 20:05:48 -0300

    matriz

--- aula 18/0001.py
+++
@@ -1,8 +0,0 @@
-for p in galera:
-    if p[1] >=21:
-        print(f'{p[0]} é maior de idade.')
-        totmai += 1
-    else:
-        print(f'{p[0]} é menor de idade.')
-        totmen += 1
-print(f'Temos {totmai} maiores e {totmen} menores de idade' )
