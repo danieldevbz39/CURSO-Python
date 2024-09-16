66d4862a5d727cef6386e5f2a52d84aab1dd0cfd
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2024-09-16 13:12:14 -0300

    at16/09
    Calcular área
    print
    contador
    valor

---
+++ aula 20/ex097 Um print especial.py
@@ -0,0 +1,9 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'    {msg}')
+    print('~' * tam)
+
+
+escreva('Daniel dos santos Siqueira')
+escreva('Aprendendo Python')
