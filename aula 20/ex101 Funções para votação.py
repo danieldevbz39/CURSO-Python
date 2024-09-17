bd3b6f5139a4fb9cc64b8f771bb2a2778c0c466c
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2024-09-16 22:03:23 -0300

    ex100 e 101
    funções e parâmetros

---
+++ aula 20/ex101 Funções para votação.py
@@ -0,0 +1,78 @@
+def contador(i, f, p):
+    """
+    -> Faz uma contagem e mostra na tela.
+    :param i: início da contagem
+    :param f: fim da contagem
+    :param p: passo da contagem
+    :return: sem retorno
+    """
+    c = i
+    while c <= f:
+        print(f'{c} ', end='')
+        c += p
+    print('FIM!')
+
+
+print('~' * 30)
+contador(2, 10, 2)
+print('~' * 30)
+
+
+def soma(a=0, b=0, c=0):
+    """
+    -> Faz a soma de três valores e mostra o resultado na tela.
+    :param a: o primeiro valor
+    :param b: o segundo valor
+    :param c: o terceiro valor
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+
+soma(3, 2, 5)
+soma(15, 12, 5)
+soma(1, 2)
+soma()
+
+print('-=' * 30)
+
+
+def somar(a=0, b=0, c=0):
+    s = a + b + c
+    return s
+
+
+r1 = somar(3, 2, 5)
+r2 = somar(2, 2)
+r3 = somar(6)
+
+print(f'Os resultados foram {r1}, {r2}, {r3}')
+print('_' * 50)
+
+
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2}, e {f3}')
+print('5' * 50)
+
+
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('Não é par!')
