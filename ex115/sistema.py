ed1c87ac464b29e4d4c5a6479375a1b21d6a7bc7
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2024-09-28 20:18:38 -0300

    projeto cadastro

---
+++ ex115/sistema.py
@@ -0,0 +1,28 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
+    elif resposta == 2:
+        # Opção de Cadastrar uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        # Opção sair.
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        # Digitou um opção errada.
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(0.5)
