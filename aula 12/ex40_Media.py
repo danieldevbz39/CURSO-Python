dff521209eb760a33b7435a47f98ad80c90b8e01
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2024-10-04 17:21:51 -0300

    s2

--- aula 12/ex40_Media.py
+++ aula 12/ex40_Media.py
@@ -1,7 +1,8 @@
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
-média = (nota1 + nota2) / 2
-print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}'.format(nota1, nota2, média))
+nota3 = float(input('Terceira nota: '))
+média = (nota1 + nota2+ nota3) / 2
+print('Tirando {:.1f}, {:.1f} {:.1f}, a média do aluno é {:.1f}'.format(nota1, nota2, nota3, média))
 if 7 > média >= 5:
     print('O aluno está em RECUPERAÇÃO.')
 elif média < 5:
